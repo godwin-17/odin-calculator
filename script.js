697850cfac1d94c7fa4160ce42de2a9ec4514017
Godwin <agblezeg@gmail.com>
2022-09-13 15:57:25 +0200

    Add buttons classes and populate display functions

--- script.js
+++ script.js
@@ -17,3 +17,23 @@ function divide(a, b) {
 function operate(operator, a, b) {
 
 }
+
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button.num");
+const clearBtn = document.querySelector("button.clear");
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+
+    if (button.textContent === "Clear") {
+      display.textContent = "";
+    }
+
+    display.textContent += button.textContent;
+  });
+});
+
+
+clearBtn.addEventListener("click", () => {
+  display.textContent = "";
+});
