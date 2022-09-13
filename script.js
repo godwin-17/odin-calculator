80afa934f022aba63e958c4f10a5e5853b339a3d
Godwin <agblezeg@gmail.com>
2022-09-13 16:48:10 +0200

    Add calculator functionality

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function add (a, b) {
+  console.log("A is ", a, "B is", b);
   return a + b;
 }
 
@@ -14,13 +15,16 @@ function divide(a, b) {
   return a / b;
 }
 
-function operate(operator, a, b) {
-
-}
-
 const display = document.querySelector(".display");
 const buttons = document.querySelectorAll("button.num");
 const clearBtn = document.querySelector("button.clear");
+const operators = document.querySelectorAll(".operator");
+const equals = document.querySelector(".equals");
+
+let firstNumber;
+let secondNumber;
+let operator2;
+
 
 buttons.forEach(button => {
   button.addEventListener("click", () => {
@@ -33,6 +37,42 @@ buttons.forEach(button => {
   });
 });
 
+operators.forEach(operator => {
+  operator.addEventListener("click", () => {
+
+    firstNumber = Number(display.textContent);
+
+    display.textContent = "";
+
+    if (operator.textContent === "+") {
+      operator2 = "+";
+    } else if (operator.textContent === "-") {
+      operator2 = "-";
+    } else if (operator.textContent === "*") {
+      operator2 = "*";
+    } else if (operator.textContent === "/") {
+      operator2 = "/";
+    };
+
+  });
+});
+
+equals.addEventListener("click", () => {
+  secondNumber = Number(display.textContent);
+  console.log(firstNumber);
+  console.log("SECOND NUMBER", secondNumber);
+
+  if (operator2 === "+") {
+    display.textContent = add(firstNumber, secondNumber);
+  } else if (operator2 === "-") {
+    display.textContent = subtract(firstNumber, secondNumber);
+  } else if (operator2 === "*") {
+    display.textContent = multiply(firstNumber, secondNumber);
+  } else if (operator2 === "/") {
+    display.textContent = divide(firstNumber, secondNumber);
+  };
+
+});
 
 clearBtn.addEventListener("click", () => {
   display.textContent = "";
